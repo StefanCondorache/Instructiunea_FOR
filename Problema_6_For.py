be1fcd01f8fca94a1e42feef655b4d0f55ba92f5
StefanCondorache <70690185+StefanCondorache@users.noreply.github.com>
2020-11-03 12:42:20 +0200

    Update Problema_6_For.py

--- Problema_6_For.py
+++ Problema_6_For.py
@@ -1,7 +1,7 @@
 n=int(input("n:"))
 s1,s2,s3,s4,s5,s6=0,0,1,0,0,3                   
 factorial=1
-for x in range(1,n+1): 
+for x in range(1,n): 
     s1+=x
     s2+=(x-1)*x
     s5+=x/(x+1)
@@ -9,7 +9,7 @@ for x in range(1,n+1):
 lista1=[x for x in range(1,(n*10)+1) if x%10==0]
 for x in lista1:
   s4+=(x+2)
-for x in range(2,n+1):
+for x in range(2,n):
     factorial*=x
     s3+=factorial
 print("s1=",s1)
